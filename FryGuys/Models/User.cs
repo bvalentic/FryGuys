90f57339399480a8aed0563fef5222dfe9596aba
bvalentic <bobvalentic@quickenloans.com>
2018-11-29 13:17:42 -0500

    Implemented Bootstrap 4; added a model User.

---
+++ FryGuys/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FryGuys.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Birthday { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool GetEmail { get; set; }
+        public string Password { get; set }
+
+    }
+}
